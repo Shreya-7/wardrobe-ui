153e77acc364ea5ab4ca619c88a845b57234810f
Shreya Agarwal <a.shreya202@gmail.com>
2024-01-05 00:08:31 +0530

    feat: create item from items page

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="/">Almirah</a>
     <div>
-      <ul class="navbar-nav">
+    <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/items">Items</a>
+        <a class="nav-link" href="/items">Items</a>
         </li>
-      </ul>
+    </ul>
     </div>
-  </nav>
+</nav>
--- src/routes/items/+page.server.ts
+++ src/routes/items/+page.server.ts
@@ -1,6 +1,35 @@
-import { ItemsService, type Item } from "../../client";
+import { type Item, Time, Season, Occasion, ItemsService, type Accessory, type Bottomwear, type Footwear, type Topwear, type SinglePiece, type Underwear } from "../../client";
 
 export async function load() {
     const items: Item[] = await ItemsService.itemsGetItemsGet();
     return { items };
 }
+
+export const actions = {
+	create: async ({ cookies, request }) => {
+		const data = await request.formData();
+        // TODO model: Remove this any
+        const category: any =  data.get('category')?.toString(); 
+        var itemToBeCreated: Item = { 
+            name: getRequestField(data, 'name'),
+            image: getRequestField(data, 'image'),
+            category: category,
+            size: getRequestField(data, 'size'),
+            material: getRequestField(data, 'material'),
+            time: getRequestField(data, 'time') as Time,
+            season: getRequestField(data, 'season') as Season,
+            occasion: getRequestField(data, 'occasion') as Occasion,
+            tags: [],
+            colors: [],
+            // TODO auth: fetch user_id from cookie
+            user: "3dff0fe5-e157-4d90-9787-78c1a6e6c470"
+        };
+        ItemsService.itemsPostItemsPost(itemToBeCreated);
+	}
+};
+
+function getRequestField(data: FormData, property: string) {
+    return data.get(property)?.toString().toLowerCase();
+}
+
+type Category = (Accessory | Bottomwear | Footwear | Topwear | SinglePiece | Underwear);
