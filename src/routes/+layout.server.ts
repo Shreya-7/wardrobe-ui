5f3538cc9323875b25feff9c26ca1702b27e6895
Shreya Agarwal <a.shreya202@gmail.com>
2024-09-29 22:58:41 +0530

    fix: store user data to avoid GET calls for every page load

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,9 @@ import { OpenAPI, UsersService } from "../client";
 import { ACCESS_TOKEN, IS_USER_DATA_SET, USER_ID, USER_NAME } from "../constants";
 import type { LayoutServerLoad } from "./$types";
 
+
+// this data is being fetched here because we want the user data to be accessible to all child pages
+// adding to the `login` route layout page would not have worked since it isn't high up enough in the hierarchy
 export const load = (async ({ cookies }) => {
     const accessToken = cookies.get(ACCESS_TOKEN);
     if (!accessToken) {
